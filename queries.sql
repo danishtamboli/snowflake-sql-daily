a02b3d11c1c764ef58c4f69e00273ddc427662a2
Danish Tamboli <70634416+danishtamboli@users.noreply.github.com>
2026-04-22 06:22:34 +0530

    Get employees working in departments 1 and 2
    Get employees working in departments 1 and 2

--- queries.sql
+++ queries.sql
@@ -1,9 +1,3 @@
--- Problem: Get employees earning more than average in their department
-
-SELECT e1.name, e1.salary, e1.department_id
-FROM employees e1
-WHERE e1.salary > (
-    SELECT AVG(e2.salary)
-    FROM employees e2
-    WHERE e2.department_id = e1.department_id
-);
+SELECT name, department_id
+FROM employees
+WHERE department_id IN (1, 2);
